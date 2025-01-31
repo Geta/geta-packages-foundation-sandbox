f2d345a818f101e770296182e91b103b28f49c61
Jevgenij Puchko <jevgen.pucko@gmail.com>
2025-01-31 13:17:05 +0200

    Add required scripts, configuration

---
+++ src/Foundation.AppHost/Configurations/AppHostConfiguration.cs
@@ -0,0 +1,14 @@
+namespace Foundation.AppHost.Configurations;
+
+public class AppHostConfiguration
+{
+    public string SqlServerName { get; set; } = null!;
+    public int SqlServerPort { get; set; }
+    public string CmsDatabaseName { get; set; } = null!;
+    public string CommerceDatabaseName { get; set; } = null!;
+    public string User { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string WebName { get; set; } = null!;
+    public int WebPort { get; set; }
+    public string? WebScheme { get; set; }
+}
